3bb8b9bb3b71c270b441bf6908efd8fcc815ae8a
imshahbaz21 <imshahbaz21@gmail.com>
2025-12-31 22:50:26 +0530

    fix

--- src/api/axios.js
+++ src/api/axios.js
@@ -59,7 +59,7 @@ export const strategyAPI = {
 // Margin API methods
 export const marginAPI = {
   getAllMargins: () => api.get('/api/margin/all'),
-  loadFromCsv: (formData) =>  api.post('/api/margin/load-from-csv', formData),
+  loadFromCsv: (formData) => api.post('/api/margin/load-from-csv', formData),
 };
 
 // Config API methods
