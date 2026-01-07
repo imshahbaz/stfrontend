7209e450f8156b270742132c14615ec2a6937061
imshahbaz21 <imshahbaz21@gmail.com>
2026-01-07 11:28:54 +0530

    Revert "fix"
    This reverts commit f099b5eb0c8884f0840c2807988e37f3c0c9d73e.

--- src/components/TruecallerLogin.jsx
+++ src/components/TruecallerLogin.jsx
@@ -17,6 +17,8 @@ const TruecallerLogin = ({
     async (data) => {
       setInternalIsLoading(false);
       login(data);
+      await refreshUserData()
+      navigate('/', { replace: true });
     },
     (error) => {
       console.error('Truecaller polling error:', error);
