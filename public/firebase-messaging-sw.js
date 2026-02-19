289e0070fd5dbde68dc3bbb5a15c8b2fbaa87188
Shahbaz Ahamed <72379882+imshahbaz@users.noreply.github.com>
2026-02-19 23:13:58 +0530

    Revert "fix"

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,14 +2,13 @@
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app-compat.js');
 importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging-compat.js');
 
+const urlParams = new URL(location).searchParams;
+
 firebase.initializeApp({
-    apiKey: "AIzaSyCBu_s7-xSJHmjzicPLHCVE9rGxgeCFTas",
-    authDomain: "shahbaz-trades-96206.firebaseapp.com",
-    projectId: "shahbaz-trades-96206",
-    storageBucket: "shahbaz-trades-96206.firebasestorage.app",
-    messagingSenderId: "1042092550487",
-    appId: "1:1042092550487:web:f982bed83079c088243612",
-    measurementId: "G-M91Y2FYJ4R"
+    apiKey: urlParams.get('apiKey'),
+    projectId: urlParams.get('projectId'),
+    messagingSenderId: urlParams.get('messagingSenderId'),
+    appId: urlParams.get('appId')
 });
 
 const messaging = firebase.messaging();
